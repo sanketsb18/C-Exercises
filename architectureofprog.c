8da40be47c54c45a000159f289b5479a99bef8aa
sanketsb18 <sanketsb.18@gmail.com>
2019-11-21 12:10:19 +0530

    added first exercise

---
+++ architectureofprog.c
@@ -0,0 +1,8 @@
+#include<stdio.h>   //Header file //Preprocessor directives
+
+int main()       // Return type   //Entry point
+{
+    printf("Welcome to Codekul\n"); //statements
+
+    return 0;
+}                    //End point
