92964b4a4b987bb7f7b8150591456b50875a3d6d
sanketsb18 <sanketsb.18@gmail.com>
2019-11-22 17:17:38 +0530

    armstrong and maximum nuber

---
+++ maxnum.c
@@ -0,0 +1,43 @@
+/*#include<stdio.h>
+int main (){
+
+    int a, b, c, num;
+    printf("Enter the numbers A, B and C");
+    scanf("%d%d%d", &a, &b, &c);
+
+    if(a>=b && a>=c)
+{
+    printf("A is the maximum number.", a);
+ 
+}
+    else if (b>=a && b>=c);
+ {
+    printf("B is the maximum number.", b);
+  
+   }
+   
+     else printf("C is the maximum number.",c);
+
+    return 0;
+}
+*/
+
+
+
+
+#include <stdio.h> 
+int main() 
+{ 
+    int A, B, C, largest, max; 
+  
+    printf("Enter three numbers: "); 
+    scanf("%d %d %d", &A, &B, &C); 
+  
+    largest = A > B ? (A > C ? A : C) : (B > C ? B : C); 
+    max = (A > B && A > C) ? A :(B > C) ? B : C;
+  
+    printf("%d is the largest number.\n", largest); 
+    printf("largest number is %d\n", max);
+  
+    return 0; 
+}
