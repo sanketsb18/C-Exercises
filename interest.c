899c5a144a91fe64f9ab42ff716031a817d9a616
sanketsb18 <sanketsb.18@gmail.com>
2019-11-25 18:02:28 +0530

    simple interest exercise

---
+++ interest.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+int main()
+{ 
+ float principle, rate, sinterest;
+ int time;
+ 
+ printf("Enter Principle Amount:");
+ scanf ("%f", &principle);
+
+ printf("Enter rate %% per annum:");
+ scanf ("%f", &rate);
+
+ printf("Enter time:");
+ scanf ("%d", &time);
+ 
+ sinterest = (principle * rate * time)/ 100.0;
+ 
+ printf ("Principle Amount = %5.2f\n", principle);
+ printf ("Rate %% per Annum   = %5.2f\n", rate);
+ printf ("Time   = %d years\n", time);
+ printf ("Simple Interest  = %5.2f\n", sinterest);
+
+ return 0;
+
+}
