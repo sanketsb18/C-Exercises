a40925ffaf06f642a4d943d4d1b1ecdd410246da
sanketsb18 <sanketsb.18@gmail.com>
2019-11-25 13:34:09 +0530

    Percentage and Grades Exercise

--- marks.c
+++ marks.c
@@ -1,4 +1,5 @@
-/*Write a C program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and Computer. Calculate percentage and grade according to following:
+/*Write a C program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and Computer. 
+Calculate percentage and grade according to following:
 Percentage >= 90% : Grade A
 Percentage >= 80% : Grade B
 Percentage >= 70% : Grade C
@@ -12,6 +13,54 @@ int main ()
 
 {
 
+    int p,c,b,m,comp,total;
+    float per;
+
+    printf("Enter the marks for physics out of 100:");
+    scanf("%d",&p);
+
+    printf("Enter the marks for chemistry out of 100:");
+    scanf("%d",&c);
+
+    printf("Enter the marks for biology out of 100:");
+    scanf("%d",&b);
+
+    printf("Enter the marks for mathematics out of 100:");
+    scanf("%d",&m);
+
+    printf("Enter the marks for computer out of 100:");
+    scanf("%d",&comp);
+
+    per = (p + c + b + m + comp) / 5.0;
+
+    printf("Percentage = %.2f\n", per);
+
+
+    //grade according to the percentage 
+    if(per >= 90)
+    {
+        printf("Grade A\n");
+    }
+    else if(per >= 80)
+    {
+        printf("Grade B\n");
+    }
+    else if(per >= 70)
+    {
+        printf("Grade C\n");
+    }
+    else if(per >= 60)
+    {
+        printf("Grade D\n");
+    }
+    else if(per >= 40)
+    {
+        printf("Grade E\n");
+    }
+    else
+    {
+        printf("Grade F\n");
+    }
 
     return 0;
 }
