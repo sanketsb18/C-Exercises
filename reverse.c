19a2736bbdac1111372441ea61e64f1bd6441533
sanketsb18 <sanketsb.18@gmail.com>
2019-11-27 18:01:27 +0530

    reverse number using while loop.
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

--- reverse.c
+++ reverse.c
@@ -5,13 +5,13 @@ int main()
     int num, reverse = 0;
 
     printf("Enter any number to find the reverse:");
-    scanf("%d", &num);
+    scanf("%u", &num);
 
     while(num!=0)
     {
         reverse=(reverse*10) + (num%10);
         num/=10;
     }
-    printf("reverse number is = %d\n", reverse);
+    printf("reverse number is = %u\n", reverse);
  return 0;   
 }
