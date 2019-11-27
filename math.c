c58f9002629b338330ab1d3e98eec3cea5b5e0ce
sanketsb18 <sanketsb.18@gmail.com>
2019-11-27 12:59:08 +0530

    math library use. use filename.extension -lm  while compiling.
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

---
+++ math.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    double num = 6, squareRoot;
+    squareRoot =  sqrt(num);
+    printf("Square root of %lf =  %lf\n", num, squareRoot);
+
+    return 0;
+}
