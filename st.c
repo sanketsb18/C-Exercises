0391c52ca8fc57385ad415b26d0db107510dff9d
sanketsb18 <sanketsb.18@gmail.com>
2019-11-28 11:17:04 +0530

    string length
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

---
+++ st.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char name[50];
+    printf("Enter the name:");
+    scanf("%s", name);
+    float len;
+    len = strlen(name);
+    printf("The Length of a Given String '%s' = %.f \n", name, len);
+
+    return 0;
+}
