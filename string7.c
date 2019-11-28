09fc1b9a90ea0f6f16b0597136d274b978b83fa4
sanketsb18 <sanketsb.18@gmail.com>
2019-11-28 15:07:20 +0530

    use of concatenated function strcat
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

---
+++ string7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#define maxsize 100
+
+int main(){
+
+    char a[maxsize], b[maxsize];
+
+    printf("Enter the sring1:");
+    scanf("%s", a);
+
+    printf("Enter the sring2:");
+    scanf("%s", b);
+
+    strcat(a,b);
+
+    printf("Concatenated string is: %s\n", a);
+
+    return 0;
+}
