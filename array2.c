2d33887987265188ff11abfb77538a2d88946a9d
sanketsb18 <sanketsb.18@gmail.com>
2019-11-29 16:10:02 +0530

    array and pointers

---
+++ array2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define size 5
+
+int main(){
+    int arr[size];
+    int i;
+
+    for(i=0; i<=size; i++)
+
+    scanf("%d", &arr[i]);
+
+    return 0;
+}
---
+++ pointers.c
@@ -0,0 +1,35 @@
+/**
+ * C program to demonstrate use of pointers
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int num = 1;
+    int *ptr = &num;   // ptr points to num
+    
+    printf("Value of num   = %d \n", num);
+    printf("Address of num = %p \n\n", &num);
+
+
+    printf("Value of ptr         = %p \n", ptr);
+    printf("Address of ptr       = %p \n", &ptr);
+    printf("Value pointed by ptr = %d \n\n", *ptr);
+
+
+    /* Change the value of num directly */
+    num = 10;
+    printf("After changing value of num directly. \n");
+    printf("Value of num         = %d \n", num);
+    printf("Value pointed by ptr = %d \n\n", *ptr);
+
+
+    /* Assigns 100 at the address pointed by ptr */
+    *ptr = 100;
+    printf("After changing value pointed by ptr. \n");
+    printf("Value of num         = %d \n", num);
+    printf("Value pointed by ptr = %d \n", *ptr);
+
+    return 0;
+}
