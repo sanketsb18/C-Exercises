3fca50e988baa93c7e6f0e7c38b1b66eb48d556d
sanketsb18 <sanketsb.18@gmail.com>
2019-12-02 15:09:14 +0530

    strings
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

--- string3.c
+++ string3.c
@@ -5,10 +5,13 @@
 
 int main(){
     char name[50];
+
     printf("Enter the name:");
     scanf("%s", name);
+    
     float len;
     len = strlen(name);
+    
     printf("The Length of a Given String '%s' = %.f \n", name, len);
 
     return 0;
