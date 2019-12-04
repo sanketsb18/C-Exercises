803195449090d179350072182f677a93c6d91341
sanketsb18 <sanketsb.18@gmail.com>
2019-12-04 17:51:47 +0530

    program to reverse the digits of a given number.
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

--- reverse.c
+++ reverse.c
@@ -1,16 +1,18 @@
+/* program to reverse the digits of a given number. */
+
 #include<stdio.h>
 int main()
 
 {
-    int num, reverse = 0;
+    int num, reverse = 0;       // initialize reverse number = 0
 
     printf("Enter any number to find the reverse:");
     scanf("%u", &num);
 
     while(num!=0)
     {
-        reverse=(reverse*10) + (num%10);
-        num/=10;
+        reverse=(reverse*10) + (num%10);        // reverse number + last digit
+        num/=10;                                // remove last digit
     }
     printf("reverse number is = %u\n", reverse);
  return 0;   
