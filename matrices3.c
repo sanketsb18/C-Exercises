4f779ac5c2eefc4ae87b5c200a02a91f1e6cb9ee
sanketsb18 <sanketsb.18@gmail.com>
2019-12-11 11:32:41 +0530

    program to find difference of two matrices
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

---
+++ matrices3.c
@@ -0,0 +1,73 @@
+/* program to find difference of two matrices of size n. */
+
+#include<stdio.h>
+
+#define size 100
+
+int main(){
+    int a[size][size];
+    int b[size][size];
+    int diff[size][size];
+
+    int r,c,i,j;
+
+    printf("Enter the number of rows (between 1 and 100): ");
+    scanf("%d", &r);
+    printf("Enter the number of columns (between 1 and 100): ");
+    scanf("%d", &c);
+    printf("\nEnter elements of 1st matrix:\n");
+    for (i = 0; i < r; i++){
+        for (j = 0; j < c; j++){
+            printf("Enter element a%d%d: ", i + 1, j + 1);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("Enter elements of 2nd matrix:\n");
+    for (i = 0; i < r; i++){
+        for (j = 0; j < c; j++){
+            printf("Enter element a%d%d: ", i + 1, j + 1);
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    //Display matrix a and b.
+    printf("\nMatrix a : \n");
+    for(i = 0; i < r; i++){
+        for (j = 0; j < c; j++){
+            printf("%d   ", a[i][j]);
+            if (j == c - 1){
+                printf("\n\n");
+            }
+        }
+    }
+
+    printf("\nMatrix b : \n");
+    for(i = 0; i < r; i++){
+        for (j = 0; j < c; j++){
+            printf("%d   ", b[i][j]);
+            if (j == c - 1){
+                printf("\n\n");
+            }
+        }
+    }
+
+    // substract two matrices
+    for (i = 0; i < r; i++){
+        for (j = 0; j < c; j++){
+            diff[i][j] = a[i][j] - b[i][j];
+        }
+    }
+    // printing the result
+    printf("\nDifference of two matrices: \n");
+    for (i = 0; i < r; i++){
+        for (j = 0; j < c; j++){
+            printf("%d   ", diff[i][j]);
+            if (j == c - 1){
+                printf("\n\n");
+            }
+        }
+
+    }
+        
+    return 0;
+}
