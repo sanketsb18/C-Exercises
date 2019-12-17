ff3715a0d60b63744e47ab5f3168cd5ae67b8786
sanketsb18 <sanketsb.18@gmail.com>
2019-12-17 12:50:47 +0530

    Find the cube of given number using function.
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

---
+++ cube.c
@@ -0,0 +1,24 @@
+/*Find the cube of given number.*/
+
+#include<stdio.h>
+
+double cube(double num);
+
+int main()
+
+{
+int num;
+double c;
+printf("Enter the number:");
+scanf("%d", &num);
+
+c=cube(num);
+printf("Cube of %d is %.2f\n",num, c);
+    return 0;
+}
+
+double cube(double num)
+{
+return(num*num*num);
+
+}
