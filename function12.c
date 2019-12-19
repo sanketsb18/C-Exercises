efbe3fe832e0a8c90fbfa6ba245e21bfa116b0db
sanketsb18 <sanketsb.18@gmail.com>
2019-12-19 13:57:34 +0530

    Anagram with and without function.
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

--- function12.c
+++ function12.c
@@ -1,49 +1,55 @@
-/* Write a program in C to input any two string from user at run-time
- and check whether the given two strings are Anagram or Not */
+/* program to check whether the string is anagram or not not. */
 
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 #define size 100
-int main()
-{
+
+int anagram(char *str1, char *str2);
+int main(){
     char str1[size], str2[size];
-    int len, len1, len2, i, j, found=0, not_found=0;
 
-    printf("Enter first string: ");
+    printf("Enter the string 1 : ");
     scanf("%s", str1);
-    printf("Enter second string: ");
-    scanf("%s", str2);
-    
-    len1 = strlen(str1);
-    len2 = strlen(str2);
 
-    if(len1 == len2)
-    {
-        len = len1;
-        for(i=0; i<len; i++)
-        {
-            found = 0;
-            for(j=0; j<len; j++)
-            {
-                if(str1[i] == str2[j])
-                {
-                    found = 1;
-                    break;
-                }
-            }
-            if(found == 0)
-            {
-                not_found = 1;
-                break;
-            }
-        }
-        if(not_found == 1)
-            printf("\nStrings are not Anagram\n");
-        else
-            printf("\nStrings are Anagram\n");
+    printf("Enter the string 2 : ");
+    scanf("%s", str2);
+
+
+    if(anagram(str1, str2) == 1){
+       printf(" %s and %s are Anagram.\n\n",str1,str2);
+    } 
+    else{
+       printf(" %s and %s are not Anagram.\n\n",str1,str2);
     }
-    else
-        printf("\nBoth string must contain same number of character to be an Anagram Strings\n");
 
     return 0;
+}
+
+//Function to check whether two passed strings are anagram or not
+int anagram(char str1[], char str2[]){
+    
+    int first[26] = {0}, second[26] = {0}, c=0;
+
+    // Calculating frequency of characters of the first string
+
+    while (str1[c] != '\0'){
+    first[str1[c]-'a']++;
+    c++;
+    }
+
+    c = 0;
+
+    while (str2[c] != '\0'){
+        second[str2[c]-'a']++;
+        c++;
+    }
+
+    // Comparing the frequency of characters
+
+    for (c = 0; c < 26; c++)
+        if (first[c] != second[c])
+        return 0;
+
+    return 1;
 }
