1aee0d202eff15806096f9ff1c854c5bd43daaaf
sanketsb18 <sanketsb.18@gmail.com>
2019-12-28 14:53:45 +0530

    Program to remove or delete a file.
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

---
+++ file6.c
@@ -0,0 +1,23 @@
+/* Program to remove or delete a file. */
+
+#include<stdio.h>
+
+int main (){
+    int status;
+	char fname[20];
+
+	printf("\n\n Remove a file from the disk :\n");
+	printf("----------------------------------\n"); 	
+	printf(" Input the name of file to delete : ");
+	scanf("%s",fname);
+
+	status=remove(fname);
+
+	if(status==0){
+		printf(" The file %s is deleted successfully..!!\n\n",fname);
+	}
+	else{
+        printf(" Unable to delete file %s\n\n",fname);
+	}
+    return 0;
+}
