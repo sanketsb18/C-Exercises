7f2a8965af2642535aad9afc6f784ce6302ab93d
sanketsb18 <sanketsb.18@gmail.com>
2020-01-02 15:08:21 +0530

    program to calculate the diameter, circumference and area of circle using functions.
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

---
+++ function20.c
@@ -0,0 +1,40 @@
+/* program to calculate the diameter, circumference and area of circle using functions. */
+
+#include<stdio.h>
+#include<math.h>
+
+/* declaration of functions */
+
+double getDiameter(double radius);         //function to find diameter
+double getCircumference(double radius);    //function to find circumference
+double getArea(double radius);             //function to find radius
+
+int main(){
+
+    float radius, D,C,A;
+    printf("Enter the radius:");
+    scanf("%f", &radius);
+
+    D = getDiameter(radius);
+    C = getCircumference(radius);
+    A = getArea(radius);
+
+    printf("Diameter of the circle: %.2f units\n", D);
+    printf("Circumference of the circle: %.2f units\n", C);
+    printf("Area of the circle: %.2f sq. units\n", A);
+    
+    return 0;
+}
+
+
+double getDiameter(double radius){
+    return(2*radius);
+}
+
+double getCircumference(double radius){
+    return(2*M_PI*radius);
+}
+
+double getArea(double radius){
+    return(M_PI*radius*radius);
+}
