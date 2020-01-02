701dc07317f8499bd6acccf39316922930764f5c
sanketsb18 <sanketsb.18@gmail.com>
2020-01-02 12:52:27 +0530

    program  to swap two numbers using function.
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

--- function16.c
+++ function16.c
@@ -1,15 +1,38 @@
-/* Function pointer example - Swap values */
+/* 
+Write a program in C to swap two numbers using function.
+
+Test Data :
+Input 1st number : 2
+Input 2nd number : 4
+
+Expected Output :
+Before swapping: n1 = 2, n2 = 4
+After swapping: n1 = 4, n2 = 2 
+*/
 
 #include<stdio.h>
-void swap (int *a, int *b);
+void swap (int *a, int *b);         //function declaration
 int main() {
-    int m = 25;
-    int n = 100;
-    printf("m is %d, n is %d\n", m, n);
-    swap(&m, &n);
-    printf("m is %d, n is %d\n", m, n);
+    int n1, n2;
+
+    /*Take input from user*/
+
+    printf("Input 1st number : ");
+    scanf("%d", &n1);
+                                        
+    printf("Input 2nd number : ");
+    scanf("%d", &n2);
+
+    printf("Before swapping : n1 = %d , n2 = %d\n", n1, n2);
+
+    swap(&n1, &n2);     //function call
+
+    printf("After swapping : n1 = %d , n2 = %d\n", n1, n2);
 return 0;
 }
+
+/* function definition */
+
 void swap (int *a, int *b) {
     int temp;
     temp = *a;
