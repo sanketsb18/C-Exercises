743b95720d6325b3013ce5831bfd989095eeee6c
sanketsb18 <sanketsb.18@gmail.com>
2020-01-03 18:22:19 +0530

    Program to convert binary number to octal number.
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

--- function24.c
+++ function24.c
@@ -6,11 +6,11 @@ int Binary_To_Octal(int x);         //declaration of Binary_To_Octal() function
  
 int main()
 {
-    long int binarynum, result;
+    long int binaryNum, result;
     printf("Enter the value for  binary number: ");
-    scanf("%ld", &binarynum);
+    scanf("%ld", &binaryNum);
 
-    result = Binary_To_Octal(binarynum);        //function call
+    result = Binary_To_Octal(binaryNum);        //function call
     
     printf("Equivalent octal value: %lo\n", result);
     return 0;
@@ -19,12 +19,12 @@ int main()
 /* function definition */
 
 int Binary_To_Octal(int x){
-    long int binarynum, octalnum = 0, j = 1, remainder;
+    long int binaryNum, octalNum = 0, j = 1, remainder;
     while (x != 0){
         remainder = x % 10;
-        octalnum = octalnum + remainder * j;
+        octalNum = octalNum + remainder * j;
         j = j * 2;
         x = x / 10;
     }
-    return (octalnum);
+    return (octalNum);
 }
