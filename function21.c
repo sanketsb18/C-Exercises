4b9390dcb5e33544e75b495bf822f32ff50af822
sanketsb18 <sanketsb.18@gmail.com>
2020-01-03 12:06:41 +0530

    program to convert decimal number to binary number using the function.
    Signed-off-by: sanketsb18 <sanketsb.18@gmail.com>

--- function21.c
+++ function21.c
@@ -9,29 +9,31 @@
 
 #include<stdio.h>
 
-long findBinary(int decimal_Num);
+long findBinary(int decimal_Num);       //findBinary function declaration.
 
 int main(){  
     int decimal_Num;
     long binary_Num;    
 
-    printf("Enter a decimal number : ");    
+    printf("Input any decimal number : ");    
     scanf("%d",&decimal_Num);    
 
-    binary_Num = findBinary(decimal_Num);
+    binary_Num = findBinary(decimal_Num);       //findBinary function call
+
     printf("\n The Binary value of %d is : %ld\n\n",decimal_Num, binary_Num);
 
     return 0;  
 }
 
+/* findBinary function definition */
 long findBinary(int decimal_Num)
 {
-    long binary_Num=0,remainder,i=1;
+    long binary_Num=0,remainder,base=1;
     while(decimal_Num != 0)
     {
          remainder = decimal_Num % 2;
-         binary_Num += remainder * i;
-         i *= 10;
+         binary_Num += remainder * base;
+         base *= 10;
          decimal_Num = decimal_Num / 2;
     }
     return binary_Num;
